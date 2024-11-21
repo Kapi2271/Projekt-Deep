c86782d8c6e27dc25084d2b16ac4be8b61cd5fe7
Jan U <122455310+juscilko@users.noreply.github.com>
2024-11-21 13:01:20 +0100

    Add files via upload

---
+++ Poprawnie w typescript/components/body/Title.tsx
@@ -0,0 +1,13 @@
+import Romb from '../images/romb.png'
+
+function Title() {
+  return (
+    <section>
+        <img className="rombus" id="rombus" alt="rombus" src={Romb}/>
+        <p className="shopHeader" id="shopHeader">SHOP</p>
+        <img className="rombus" id="rombus" alt="rombus" src={Romb}></img>
+    </section>
+  )
+}
+
+export default Title
