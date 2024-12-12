f3a4573ca80ff147460fa2f5089dd3d0db199c77
Jan U <122455310+juscilko@users.noreply.github.com>
2024-12-12 13:35:26 +0100

    Add files via upload

---
+++ projekt-deep nowy.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2024 at 01:35 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projekt-deep`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `items`
+--
+
+CREATE TABLE `items` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `image` varchar(50) NOT NULL,
+  `typeID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`ID`, `name`, `price`, `userID`, `image`, `typeID`) VALUES
+(1, 'Crypt Blade', 2137, 1, 'cryptblade.png', 2),
+(2, 'Hero Sword', 3450, 2, 'herosword.png', 2),
+(3, 'Ethereal Armor', 5000, 3, 'etherealarmor.png', 1),
+(4, 'Mystic Scroll', 1200, 2, 'mysticscroll.png', 1),
+(5, 'Void Crystal', 1500, 4, 'voidcrystal.png', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `types`
+--
+
+CREATE TABLE `types` (
+  `ID` int(11) NOT NULL,
+  `typeName` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`ID`, `typeName`) VALUES
+(1, 'Item'),
+(2, 'Weapon'),
+(3, 'Accessory');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `discordName` varchar(50) NOT NULL,
+  `userLogin` varchar(50) NOT NULL,
+  `userPassword` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `username`, `discordName`, `userLogin`, `userPassword`) VALUES
+(1, 'Wojtek', 'Spell', 'spell123', 'haslo123'),
+(2, 'Anna', 'MageQueen', 'anna2024', 'password1'),
+(3, 'Krzysztof', 'ShadowSlayer', 'krzysio99', 'qwerty123'),
+(4, 'Zofia', 'FlameWarden', 'zofia_flame', 'securepass45');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `typeID` (`typeID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indeksy dla tabeli `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `userLogin` (`userLogin`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `types`
+--
+ALTER TABLE `types`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`typeID`) REFERENCES `types` (`ID`),
+  ADD CONSTRAINT `items_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
